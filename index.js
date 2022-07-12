4d1a2a0ef9c222fa33660f5c1d6a8e75a93645ac
Lucas Saaler <lucassaaler00@gmail.com>
2022-07-12 00:00:55 -0400

    Added README template and skeleton for Inquirer questions

--- index.js
+++ index.js
@@ -1,13 +1,40 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown')
+const fs = require('fs')
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project's title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "What is your project's description?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How do you install your project?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Pick one from the following licenses.",
+        choices:["MIT", "IBM", "Apache"]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+        console.log(data)
+        fs.writeFileSync("./output/README.md", generateMarkdown(data))
+    })
+}
 
-// Function call to initialize app
 init();
